f624e7504ff0072b5c0351bf94d959856ae5fee1
Hélio Fernandes <104803451+HelioAnacronista@users.noreply.github.com>
2023-01-12 10:46:47 -0300

    Consumindo API do Stripe

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,29 +1,58 @@
-import { SubscribeButton } from '../components/SubscribeButton';
-import styles from './home.module.scss';
+import { GetServerSideProps, GetStaticProps } from "next";
+
+import styles from "./home.module.scss";
 
 import Heade from "next/head";
+import { SubscribeButton } from "../components/SubscribeButton";
+import { stripe } from "../services/stripe";
 
-export default function Home() {
+interface HomeProps {
+  product: {
+    priceId: string;
+    amount: number;
+  }
+}
+
+export default function Home({ product }: HomeProps) {
   return (
     <>
       <Heade>
         <title>Home | ig.news</title>
       </Heade>
-      
+
       <main className={styles.contentContainer}>
         <section className={styles.hero}>
-            <span>👏 Hey, welcome</span>
-            <h1>News about the <span>React</span> world.</h1>
-            <p>
-              Get access to all the publications <br />
-              <span>for $9.90 month</span>
-
-            </p>
-            <SubscribeButton />
+          <span>👏 Hey, welcome</span>
+          <h1>
+            News about the <span>React</span> world.
+          </h1>
+          <p>
+            Get access to all the publications <br />
+            <span>for {product.amount} month</span>
+          </p>
+          <SubscribeButton priceId={product.priceId} />
         </section>
 
         <img src="/images/avatar.svg" alt="girl coding" />
       </main>
     </>
   );
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const price = await stripe.prices.retrieve('price_1MPQQSFq3MJVu9cb828ccQIl')
+
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD'
+    }).format(price.unit_amount / 100),
+  }
+  return {
+    props: {
+      product,
+    },
+    revalidate: 60 * 60 * 24, //24 hours
+  }
 }
